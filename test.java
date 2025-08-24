3f4cc1d7256981e946d5a610ef29fbfc7d2f204e
Kelly Roduta <kellyroduta961@gmail.com>
2025-08-24 21:26:35 +0800

    java D1

---
+++ test.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class test {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Hello World");
+
+        int hello = 1;
+        int hatdog = 3;
+
+            System.out.println(hello + hatdog);
+        
+        int age = 0;
+
+        System.out.println("Please Enter Your Age: ");
+
+       age = input.nextInt();
+
+       System.out.println("You Age is " + age);
+    }
+
+
+}
